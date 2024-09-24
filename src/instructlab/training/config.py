4bb48621fcd9217ad5fbb573edd69aa38a15253c
Jaideep Rao <jrao@redhat.com>
2024-09-24 09:52:16 -0400

    add FSDP options
    Signed-off-by: Jaideep Rao <jrao@redhat.com>

--- src/instructlab/training/config.py
+++ src/instructlab/training/config.py
@@ -112,6 +112,14 @@ class DeepSpeedOptions(BaseModel):
 
 
 # public API
+class FSDPOptions(BaseModel):
+    """
+    Represents the options for configuring FSDP which are exposed by the Training Library
+    """
+
+    cpu_offload_params: Optional[bool] = False
+
+# public API
 class TrainingArgs(BaseModel):
     """
     This class represents the arguments being used by the training script.
@@ -155,6 +163,12 @@ class TrainingArgs(BaseModel):
             cpu_offload_optimizer=False,
             cpu_offload_optimizer_ratio=1,
             cpu_offload_optimizer_pin_memory=False,
+        )
+    )
+
+    fsdp_options: FSDPOptions = Field(
+        default_factory=lambda: FSDPOptions(
+            cpu_offload_params=False
         )
     )
 
